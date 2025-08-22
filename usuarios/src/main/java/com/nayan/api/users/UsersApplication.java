d2acb63d639c0ef8ef50a9da4216fc26f79394a5
Nayan Becker <146501593+NayanBecker@users.noreply.github.com>
2025-08-22 16:15:19 +0000

    docker build

--- usuarios/src/main/java/com/nayan/api/users/UsersApplication.java
+++ usuarios/src/main/java/com/nayan/api/users/UsersApplication.java
@@ -3,10 +3,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsuariosApplication {
+public class UsersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsuariosApplication.class, args);
+		SpringApplication.run(UsersApplication.class, args);
 	}
 
 }
--- usuarios/src/main/java/com/nayan/api/users/repository/UserRepository.java
+++ usuarios/src/main/java/com/nayan/api/users/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.nayan.api.users.repository;
 import com.nayan.api.users.entities.UsersEntity;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UsersEntity, UUID> {
 }

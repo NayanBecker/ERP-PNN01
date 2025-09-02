c36e09ddbbe0064f04f5ecd5fb43a036e78a3ab3
nayanbecker <nayan@unochapeco.edu.br>
2025-09-01 21:30:10 -0300

    refactor: Reorganize imports and enhance OrderCreatedListener constructor

--- pedidos/src/main/java/com/nayan/api/pedidos/listener/OrderCreatedListener.java
+++ pedidos/src/main/java/com/nayan/api/pedidos/listener/OrderCreatedListener.java
@@ -6,18 +6,27 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
-import com.nayan.api.pedidos.listener.dto.OrderCreatedEvent;
-
 import static com.nayan.api.pedidos.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+import com.nayan.api.pedidos.listener.dto.OrderCreatedEvent;
+import com.nayan.api.pedidos.services.OrderService;
 
 @Component
 public class OrderCreatedListener {
 
+    private final OrderService orderService;
+
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
         OrderCreatedEvent event = message.getPayload();
         logger.info("Received order created event: {}", event);
+
+        orderService.saveOrder(message.getPayload());
     }
+
 }
